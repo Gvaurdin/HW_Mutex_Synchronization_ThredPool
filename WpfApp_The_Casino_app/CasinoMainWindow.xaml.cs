c9b3a81fbc0c20d4090a7ca7457ef0426a3c10c6
Gvaurdin <gvaurdin777@yandex.ru>
2024-04-15 22:15:24 +0300

    The operation of the streams is fully configured and the logic of the casino game is implemented

--- WpfApp_The_Casino_app/CasinoMainWindow.xaml.cs
+++ WpfApp_The_Casino_app/CasinoMainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,13 +23,10 @@ namespace WpfApp_The_Casino_app
     /// </summary>
     public partial class CasinoMainWindow : Window
     {
-        static Dictionary<Label, Label> places = new Dictionary<Label, Label>()
-        {
 
-        };
         private const int max_playesrs = 5;
-        private const int min_all_players = 20;
-        private const int max_all_players = 100;
+        private const int min_all_players = 5;
+        private const int max_all_players = 5;
         private const int max_money = 1000;
         private static int count = 0;
         private static int countStuffNumber = 0;
@@ -36,14 +34,8 @@ namespace WpfApp_The_Casino_app
         private static int round = 0;
         private readonly object lockObj;
         private static bool isRunGame = false;
-
-        public event Action<bool> threwNumber;
         public event Action<string> NewEvent;
-        public event Action NextRound;
-        public event Action finishRound;
-        public event Action<Player> playerGameOver;
-
-        Thread[] playersThreads;
+        List<Thread> playersThreads;
         private Random random;
         private int waitingPlayers;
         private static List<Player> players;
@@ -52,131 +44,258 @@ namespace WpfApp_The_Casino_app
         {
             InitializeComponent();
             random = new Random();
-            selectNumber = new int[5]; 
-            playersThreads = new Thread[5];
+            selectNumber = new int[5];
+            playersThreads = new List<Thread>();
             lockObj = new object();
-            ControlsShow(false);
             players = new List<Player>();
             NewEvent += CasinoMainWindow_NewEvent;
-            playerGameOver += AddNewPlayer;
-            threwNumber += CheckBoolThrew;
-            finishRound += CasinoMainWindow_finishRound;
-            NextRound += Game;
         }
+
 
         private void CasinoMainWindow_finishRound()
         {
             numberCasino = selectNumber[random.Next(selectNumber.Length)];
-            for (int i = 0; i < players.Count; i++) 
+            for (int i = 0; i < players.Count; i++)
             {
-                if (players[i].throwNumber == numberCasino) 
+                if (players[i].throwNumber == numberCasino)
                 {
                     NewEvent($"Player[{players[i].name}] is win {players[i].moneyStuff}.$");
+                    if (players[i].moneyStuff > 200) players[i].Money += players[i].moneyStuff + 200;
+                    else
+                    {
+                        players[i].Money += players[i].moneyStuff;
+                    }
+                    Thread.Sleep(100);
                 }
                 else
                 {
                     NewEvent($"Player[{players[i].name}] is lose {players[i].moneyStuff}.$");
+                    players[i].Money -= players[i].moneyStuff;
+                    Thread.Sleep(100);
                 }
-                Thread.Sleep(3000);
             }
-            countStuffNumber = 0;
+            CheckMoneyPlayers();
             NewEvent($"Round {round} is finish");
-            NextRound();
+            if (waitingPlayers == 0 && players.Count < 2 && players.Count > 0)
+            {
+                EndGame();
+            }
+            else
+            {
+                round++;
+                countStuffNumber = 0;
+            }
         }
+
+        private void CheckMoneyPlayers()
+        {
+            try
+            {
+                lock (lockObj)
+                {
+                    List<Player> removedPlayers = players.Where(p => p.Money < 100).ToList();
+                    int countDeletePlayers = players.RemoveAll(p => p.Money < 100);
+                    foreach (Player player in removedPlayers)
+                    {
+                        NewEvent($"Player[{player.name}] with balance {player.Money} was removed.");
+                    }
+                    if (countDeletePlayers > 0 && waitingPlayers > 0)
+                    {
+                        if (countDeletePlayers <= waitingPlayers)
+                        {
+                            waitingPlayers -= countDeletePlayers;
+                            UpdateWaitingPlayers(waitingPlayers);
+                            AddPlayers(countDeletePlayers);
+                        }
+                        else
+                        {
+                            countDeletePlayers = waitingPlayers;
+                            waitingPlayers = 0;
+                            UpdateWaitingPlayers(waitingPlayers);
+                            AddPlayers(countDeletePlayers);
+                        }
+                    }
+                    else if (players.Count == 0)
+                    {
+                        Player winner = removedPlayers.OrderByDescending(p => p.Money).FirstOrDefault();
+                        NewEvent($"Winner player[{winner.name}]");
+                        isRunGame = false;
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error : {ex.Message}");
+            }
+        }
+
 
         private void CasinoMainWindow_NewEvent(string obj)
         {
+
             Dispatcher.Invoke(() =>
             {
                 listboxLogs.Items.Add(obj);
             });
         }
 
+        private void UpdateWaitingPlayers(int delPlayers)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                labelWaitingPlayers.Content = delPlayers.ToString(); 
+            });
+        }
+
         private void Game()
         {
-            if (!isRunGame)
+            try
             {
                 NewEvent("Game is start. Round 1");
-                round++;
-                isRunGame = true;
-                Thread.Sleep(3000);
+                Thread.Sleep(100);
+                while (isRunGame)
+                {
+                    if (round == 0)
+                    {
+                        for (int i = 0; i < max_playesrs; i++)
+                        {
+                            count++;
+                            int initialMoney = random.Next(500, max_money);
+                            Player player = new Player(count, initialMoney);
+                            players.Add(player);
+                            NewEvent($"Player[{player.name}] sat down at the table");
+                            Thread playerThread = new Thread(new ParameterizedThreadStart(Play));
+                            playerThread.Name = $"Thread player {players[i].name}";
+                            playersThreads.Add(playerThread);
+                            Thread.Sleep(100);
+                        }
+                        for (int i = 0; i < selectNumber.Length; i++)
+                        {
+                            selectNumber[i] = random.Next(0, 10 + 1);
+                        }
+                        NewEvent("Players, what are your bets?");
+                        PlayRound();
+                    }
+                    else
+                    {
+                        NewEvent($"Round {round}");
+                        for (int i = 0; i < selectNumber.Length; i++)
+                        {
+                            selectNumber[i] = random.Next(0, 10 + 1);
+                        }
+                        NewEvent("Players, what are your bets?");
+                        PlayRound();
+                    }
+                }
+                NewEvent("Game over");
+                SaveReportToFile();
             }
-            if (round == 1)
+            catch (Exception ex)
             {
-                for (int i = 0; i < selectNumber.Length; i++)
-                {
-                    selectNumber[i] = random.Next(0, 10 + 1);
-                }
-                NewEvent("Players, what are your bets?");
-                Thread.Sleep(3000);
-                for (int i = 0; i < playersThreads.Length; i++)
-                {
-                    playersThreads[i].Name = $"Thread player {players[i].name}";
-                    Thread.Sleep(3000);
-                    playersThreads[i].Start(players[i]);
-                }
+                MessageBox.Show(ex.Message);
             }
-            else
-            {
-                NewEvent($"Round {round}");
-                for (int i = 0; i < selectNumber.Length; i++)
-                {
-                    selectNumber[i] = random.Next(0, 10 + 1);
-                }
-                NewEvent("Players, what are your bets?");
-                for (int i = 0; i < players.Count; i++)
-                {
-                    Play(players[i]);
-                }
-                round++;
-            }
-
 
         }
 
-        private void CheckBoolThrew(bool flag)
+        private void EndGame()
         {
-            if (flag) countStuffNumber++;
+            isRunGame = false;
+            NewEvent($"Winner is player[{players[0].name}]");
+        }
 
-            if (countStuffNumber == 5)
+        private void PlayRound()
+        {
+            if(waitingPlayers == 0 && players.Count < 5 && players.Count != playersThreads.Count )
             {
-                NewEvent("The players placed bets");
-                finishRound();
+                for(int i = players.Count - 1; i < playersThreads.Count;i++)
+                {
+                    playersThreads[i].Abort();
+                    playersThreads.Remove(playersThreads[i]);
+                }
             }
+            for (int i = 0; i < playersThreads.Count; i++)
+            {
+                if (round == 0)
+                {
+                    playersThreads[i].Start(players[i]);
+                    playersThreads[i].Join();
+                }
+                else
+                {
+                    playersThreads[i] = new Thread(new ParameterizedThreadStart(Play));
+                    playersThreads[i].Name = $"Thread player {players[i].name}";
+                    playersThreads[i].Start(players[i]);
+                    playersThreads[i].Join();
+                }
+            }
+            if(countStuffNumber == playersThreads.Count)
+            CheckBoolThrew();
+
+        }
+
+        private void CheckBoolThrew()
+        {
+
+            lock (lockObj)
+            { 
+                if (countStuffNumber == playersThreads.Count)
+                {
+                    CasinoMainWindow_finishRound();
+                }
+            }
+
         }
 
         private void Play(object playerObj)
         {
             Player player = (Player)playerObj;
-            if(player.Money <= 0)
+            try
             {
-                playerGameOver(player);
+                lock (lockObj)
+                {
+                    player.throwNumber = selectNumber[random.Next(selectNumber.Length)];
+                    player.moneyStuff = random.Next(50, player.Money + 1);
+                    NewEvent($"Player[{player.name}] threw number {player.throwNumber} with a bet {player.moneyStuff}");
+                    Thread.Sleep(100);
+                    countStuffNumber++;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Thread name {Thread.CurrentThread.Name}\n Max value : {player.Money}\n Player name {player.name}");
+
             }
 
-            lock (lockObj)
-            {
-                player.throwNumber = selectNumber[random.Next(selectNumber.Length)];
-                player.moneyStuff = random.Next(50, player.Money / 2);
-                NewEvent($"Player[{player.name}] threw number {player.throwNumber} with a bet {player.moneyStuff}");
-                threwNumber(true);
 
-            }
-
-            Thread.Sleep(3000);
         }
 
 
-        private void AddNewPlayer(Player player)
+        private void AddPlayers(int countDeletePlayers)
         {
-            foreach (Player item in players)
+            lock (lockObj)
             {
-                if (item.name == player.name) players.Remove(item);
+                if(countDeletePlayers == 1)
+                {
+                    count++;
+                    Player tmp = new Player(count, random.Next(500, max_money));
+                    NewEvent($"Player[{tmp.name}] joins the game");
+                    players.Add(tmp);
+                }
+                else
+                {
+                    for (int i = 0; i < countDeletePlayers; i++)
+                    {
+                        count++;
+                        Player tmp = new Player(count, random.Next(500, max_money));
+                        NewEvent($"Player[{tmp.name}] joins the game");
+                        players.Add(tmp);
+                    }
+                }
             }
-            count++;
-            Player tmp = new Player(count, random.Next(100, max_money));
-            players.Add(tmp);
-
-
         }
         private void LogPlayerResult(Player player)
         {
@@ -201,49 +320,48 @@ namespace WpfApp_The_Casino_app
             //}
         }
 
-        private void ControlsShow(bool flag)
+        private void ControlsShow(object flag)
         {
-            if (!flag)
+            bool isRun = (bool)flag;
+            Dispatcher.Invoke(() =>
             {
-                listboxLogs.Visibility = Visibility.Hidden;
-            }
-            else if (flag)
-            {
-                listboxLogs.Visibility = Visibility.Visible;
-            }
+                if (!isRun)
+                {
+                    listboxLogs.Visibility = Visibility.Hidden;
+                }
+
+                else if (isRun)
+                {
+                    listboxLogs.Visibility = Visibility.Visible;
+                }
+            });
         }
 
         private void buttonStart_Click_1(object sender, RoutedEventArgs e)
         {
-            waitingPlayers = random.Next(min_all_players, max_all_players + 1);
-            labelWaitingPlayers.Content = waitingPlayers.ToString();
             ControlsShow(true);
-
-            for (int i = 0,count = 1; i < playersThreads.Length; i++, count++)
-            {
-                int initialMoney = random.Next(100, max_money);
-                Player player = new Player(count, initialMoney);
-                players.Add(player);
-                NewEvent($"Player[{player.name}] sat down at the table");
-                playersThreads[i] = new Thread(Play);
-            }
-            Game();
+            isRunGame = true;
+            waitingPlayers = random.Next(min_all_players, max_all_players);
+            labelWaitingPlayers.Content = waitingPlayers.ToString();
+            Thread threadMainGame = new Thread(new ThreadStart(Game));
+            threadMainGame.Name = "Game Thread";
+            threadMainGame.Start();
         }
 
-        //private void SaveReportToFile()
-        //{
-        //    string filePath = "CasinoReport.txt";
+        private void SaveReportToFile()
+        {
+            string filePath = "CasinoReport.txt";
 
-        //    using (StreamWriter writer = new StreamWriter(filePath))
-        //    {
-        //        foreach (Player player in players)
-        //        {
-        //            writer.WriteLine($"{player.Name} {player.InitialMoney} {player.Money}");
-        //        }
-        //    }
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (string line in listboxLogs.Items)
+                {
+                    writer.WriteLine(line);
+                }
+            }
 
-        //    MessageBox.Show("Simulation finished. Report saved to CasinoReport.txt");
-        //}
+            MessageBox.Show("Simulation finished. Report saved to CasinoReport.txt");
+        }
 
 
     }
